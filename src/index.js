01705f4465cd17e695bf92b8a17d8e7ab0466334
Jan Randa <randajan@email.cz>
2023-07-12 01:02:27 +0200

    NPM publish

--- src/index.js
+++ src/index.js
@@ -7,9 +7,11 @@ export const validateIBAN = iban=>{
     return validIBAN.test(iban);
 }
 
+
 export const formatIBAN = iban=>{
     if (!validateIBAN(iban)) { return ""; } 
     return String(iban).split('').reduce((a, b) => a + ((a.length + 1) % 5 === 0 ? ' ' : '') + b);
 }
+
 
 export const getIBAN = (country, bank, account, format=false)=>getCountry(country).getIBAN(bank, account, format);
